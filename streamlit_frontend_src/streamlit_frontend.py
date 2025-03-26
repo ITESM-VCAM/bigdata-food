a3ef46641adb71ff5b103c82ccd70f747f8a52bd
ITESM-VCAM <A01735632@tec.mx>
2025-03-25 20:00:02 -0600

    update frontend

--- streamlit_frontend_src/streamlit_frontend.py
+++ streamlit_frontend_src/streamlit_frontend.py
@@ -39,7 +39,8 @@ github_token = sl.text_input('Github token', value='')
 
 sl.header('Data visualization')
 file_url = sl.text_input(
-    'File URL', value=f'https://github.com/{github_user}/{github_repo}/raw/refs/heads/master/results/results.json')
+    'File URL',
+    value=f'https://github.com/{github_user}/{github_repo}/raw/refs/heads/master/results/FILENAME.json')
 if sl.button('GET dataset file'):
     get_processed_dataset(file_url)
 
